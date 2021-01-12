3cf0303fbcc85c541d6d76eba44a6a0b5116e19b
james <jaewon028@gmail.com>
2021-01-12 19:09:20 +0900

    011221 BK07_08_Dial BK5622

--- BK07_String/Dial.py
+++ BK07_String/Dial.py
@@ -1,2 +1,11 @@
 if __name__ == "__main__":
-    print("박치원 똥멍청이")
+    S = input()
+    list_alphabet = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+    dial_time = 0
+
+    for i in range(len(S)):
+        for j in list_alphabet:
+            if S[i] in j :
+                dial_time += list_alphabet.index(j) +3
+
+    print(dial_time)
