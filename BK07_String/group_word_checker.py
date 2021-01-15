06048ba664f4a576a9150c27bc595e64666dec4f
james <jaewon028@gmail.com>
2021-01-15 20:47:13 +0900

    011521 BK07_10_group_word_checker BK1316

--- BK07_String/group_word_checker.py
+++ BK07_String/group_word_checker.py
@@ -1,8 +1,9 @@
 if __name__ == "__main__":
-    num = int(input())
-    list_S = [input() for i in range(num)]
-
-    cnt = 0
-
-
-    print(cnt)
+    result = int(input())
+    for j in range(result):
+        word = input()
+        for i in range(1, len(word)):
+            if word.find(word[i - 1]) > word.find(word[i]):
+                result -= 1
+                break
+    print(result)
