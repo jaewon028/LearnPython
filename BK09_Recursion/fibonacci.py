57bd289d0e0ad36aee26adcff2d8697ca25e911e
james <jaewon028@gmail.com>
2021-01-19 23:52:14 +0900

    011921 BK09_02_fibonacci BK10872-error fixed

--- BK09_Recursion/fibonacci.py
+++ BK09_Recursion/fibonacci.py
@@ -1,8 +1,9 @@
 def fibonacci(n):
-    if n == 2 or n == 1:
+    if n == 0:
+        return 0
+    if n == 1:
         return 1
-    else:
-        return fibonacci(n-2) + fibonacci(n-1)
+    return fibonacci(n-2) + fibonacci(n-1)
 
 
 if __name__ == "__main__":
